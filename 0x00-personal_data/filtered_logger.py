e87a291d3d3d3c39a41194e115481e810ab48db1
Akinwalee <obatulafuad@gmail.com>
2024-11-08 03:30:54 +0000

    Implement data obfustcation with Regex

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Filter and replace PII data
+"""
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Filter the given by obfustacating the sepcified fields
+    """
+
+    pattern = r'(?<={})({}=[^{}]*)(?={})'.format(
+                separator, '|'.join(fields), separator)
+    repl = r'\1{}'.format(redaction)
+    new_str = re.sub(pattern, repl, message)
+
+    return (new_str)
