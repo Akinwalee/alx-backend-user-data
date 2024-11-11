ca73905adcc6783046c31b521aa6d249e22f90ec
Akinwalee <obatulafuad@gmail.com>
2024-11-11 19:10:52 +0000

    Complete custom error pages handling

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -24,3 +24,19 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized():
+    """
+    Return unauthorized
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden():
+    """
+    Return Forbidden
+    """
+    abort(403)
