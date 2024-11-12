515eb4b9794b0235600ea83633f52117860efec8
Akinwalee <obatulafuad@gmail.com>
2024-11-12 16:25:57 +0000

    Implement Base64 extraction and Decoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,7 @@
 """
 Basic Auth Implementation
 """
-
+from base64 import b64decode
 from api.v1.auth.auth import Auth
 
 
@@ -10,4 +10,28 @@ class BasicAuth(Auth):
     """
     Basic Auth
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Extract base64 from header
+        """
+        header = authorization_header
+        if not header or not isinstance(header, str) or not header.startswith(
+                "Basic "):
+            return None
+
+        return header.split()[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decode Base64
+        """
+        header = base64_authorization_header
+        if not header or not isinstance(header, str):
+            return None
+
+        try:
+            return b64decode(header).decode("utf-8")
+        except Exception:
+            return None
